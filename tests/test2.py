a2a99f672d4156f404422efd02a3574a236be517
Srikanth927 <srikanth@elequincapital.com>
2023-12-29 02:36:58 +0530

    unittests

--- tests/test2.py
+++ tests/test2.py
@@ -2,10 +2,49 @@
 
 from mypkg.mod2 import sqr, add, divs
 
-print(sqr(123))
+import unittest
 
-print(divs(588, 324))
+def add(a, b):
+    c = a + b
+    return c
 
-print(add(2123, -5))
+def sqr(a):
+    c = a ** 2
+    return c
 
-print(add(2334,1234))
+def divs(a, b):
+    c = a / b
+    return c
+
+class TestMyFunctions(unittest.TestCase):
+
+    def test_add_positive_numbers(self):
+        result = add(5, 3)
+        self.assertEqual(result, 8, "Addition of positive numbers failed")
+
+    def test_add_negative_numbers(self):
+        result = add(-5, 3)
+        self.assertEqual(result, -2, "Addition of negative and positive numbers failed")
+
+    def test_sqr_positive_number(self):
+        result = sqr(4)
+        self.assertEqual(result, 16, "Square of a positive number failed")
+
+    def test_sqr_negative_number(self):
+        result = sqr(-3)
+        self.assertEqual(result, 9, "Square of a negative number failed")
+
+    def test_divs_integer_division(self):
+        result = divs(10, 2)
+        self.assertEqual(result, 5, "Integer division failed")
+
+    def test_divs_float_division(self):
+        result = divs(7, 3)
+        self.assertAlmostEqual(result, 2.333, places=3, msg="Float division failed")
+
+    def test_divs_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divs(5, 0)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test3.py
+++ tests/test3.py
@@ -1,7 +1,42 @@
 # Test Case 3: Import mod2.py
 
 from mypkg.mod1 import sub,func
+import unittest
 
-print(sub(1395, 222))
+def sub(a, b):
+    c = a - b
+    return c
 
-print(func([22,11,3,1,2,2,1]))
+def func(l):
+    if len(l)==0:
+        return 0
+    else:
+        return l.count(min(l))
+class TestMyFunctions(unittest.TestCase):
+
+    def test_sub_positive_numbers(self):
+        result = sub(5, 2)
+        self.assertEqual(result, 3, "Subtraction of positive numbers failed")
+
+    def test_sub_negative_numbers(self):
+        result = sub(-5, -2)
+        self.assertEqual(result, -3, "Subtraction of negative numbers failed")
+
+    def test_sub_mixed_numbers(self):
+        result = sub(5, -2)
+        self.assertEqual(result, 7, "Subtraction of mixed numbers failed")
+
+    def test_func_empty_list(self):
+        result = func([])
+        self.assertEqual(result, 0, "Function with an empty list failed")
+
+    def test_func_single_element_list(self):
+        result = func([7,2])
+        self.assertEqual(result, 1, "Function with a single-element list failed")
+
+    def test_func_multiple_element_list(self):
+        result = func([3, 1, 2, 3, 1, 3])
+        self.assertEqual(result, 2, "Function with multiple-element list failed")
+
+if __name__ == '__main__':
+    unittest.main()
